ccfa9f796b2b47fbc252dd856724a78b05d18cec
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-06-17 06:36:35 +0000

    array

---
+++ Arrays_05/FindMaxValueFromArray_06.java
@@ -0,0 +1,23 @@
+package Arrays_05;
+import java.util.Scanner;
+
+public class FindMaxValueFromArray_06{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of elements you want to add in the array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i< n ;i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = -2147483; 
+        for(int i = 0; i< n ;i++){
+            if(arr[i] > max ){
+                max = arr[i];
+            }
+        }
+        System.out.println("Max value is : " + max);
+    }
+}
